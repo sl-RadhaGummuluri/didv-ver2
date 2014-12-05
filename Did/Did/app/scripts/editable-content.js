c269ed6cd22b713a4fcb7e8978364a7526e5ba00
SL-LauraMihelich <lmihelich@shoplocal.com>
2014-12-05 10:33:59 -0600

    hooking up editing! yay!

--- Did/Did/app/scripts/editable-content.js
+++ Did/Did/app/scripts/editable-content.js
@@ -11,3 +11,101 @@ $('.save').click(function(){
   $('.text').removeAttr('contenteditable');
   $('.edit').show();
 });
+
+$('.edit2').click(function () {
+    $(this).hide();
+    $('.box2').addClass('editable');
+    $('.text').attr('contenteditable', 'true');
+    $('.save2').show();
+});
+
+$('.save2').click(function () {
+    $(this).hide();
+    $('.box2').removeClass('editable');
+    $('.text').removeAttr('contenteditable');
+    $('.edit2').show();
+});
+
+$('.edit3').click(function () {
+    $(this).hide();
+    $('.box3').addClass('editable');
+    $('.text').attr('contenteditable', 'true');
+    $('.save3').show();
+});
+
+$('.save3').click(function () {
+    $(this).hide();
+    $('.box3').removeClass('editable');
+    $('.text').removeAttr('contenteditable');
+    $('.edit3').show();
+});
+
+$('.edit4').click(function () {
+    $(this).hide();
+    $('.box4').addClass('editable');
+    $('.text').attr('contenteditable', 'true');
+    $('.save4').show();
+});
+
+$('.save4').click(function () {
+    $(this).hide();
+    $('.box4').removeClass('editable');
+    $('.text').removeAttr('contenteditable');
+    $('.edit4').show();
+});
+
+$('.edit5').click(function () {
+    $(this).hide();
+    $('.box5').addClass('editable');
+    $('.text').attr('contenteditable', 'true');
+    $('.save5').show();
+});
+
+$('.save5').click(function () {
+    $(this).hide();
+    $('.box5').removeClass('editable');
+    $('.text').removeAttr('contenteditable');
+    $('.edit5').show();
+});
+
+$('.edit6').click(function () {
+    $(this).hide();
+    $('.box6').addClass('editable');
+    $('.text').attr('contenteditable', 'true');
+    $('.save6').show();
+});
+
+$('.save6').click(function () {
+    $(this).hide();
+    $('.box6').removeClass('editable');
+    $('.text').removeAttr('contenteditable');
+    $('.edit6').show();
+});
+
+$('.edit7').click(function () {
+    $(this).hide();
+    $('.box7').addClass('editable');
+    $('.text').attr('contenteditable', 'true');
+    $('.save7').show();
+});
+
+$('.save7').click(function () {
+    $(this).hide();
+    $('.box7').removeClass('editable');
+    $('.text').removeAttr('contenteditable');
+    $('.edit7').show();
+});
+
+$('.edit8').click(function () {
+    $(this).hide();
+    $('.box8').addClass('editable');
+    $('.text').attr('contenteditable', 'true');
+    $('.save8').show();
+});
+
+$('.save8').click(function () {
+    $(this).hide();
+    $('.box8').removeClass('editable');
+    $('.text').removeAttr('contenteditable');
+    $('.edit8').show();
+});
